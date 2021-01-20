c4898ebe773867bb363b6a082d670ca76cec439e
meishimeike <39954865+meishimeike@users.noreply.github.com>
2021-01-20 20:01:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,6 +18,10 @@ sed -i 's/19375-1-53475/21490-1-58574/g' package/firmware/cypress-firmware/Makef
 sed -i 's/firmware\/brcmfmac/firmware\/cyfmac/g' package/firmware/cypress-firmware/Makefile
 sed -i 's/b12b0570f462c2f3c26dde98b10235a845a7109037def1e7e51af728bcc1a958/fb71c344e705f5bc9fdae3ce0fbfa299f0af0939ff3ec782aeca0308911d830d/g' package/firmware/cypress-firmware/Makefile
 
+# Add ChinaDNS-NG
+git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
+git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
+
 # Add smartdns
 svn co https://github.com/pymumu/smartdns/trunk/package/openwrt package/smartdns
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns package/luci-app-smartdns
